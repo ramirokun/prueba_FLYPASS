e7c5e98e9144e91a37b29f5de9fe4d89122c3ea1
Ramiro Meneses <ramiro052205@gmail.com>
2025-02-16 13:09:35 -0500

    agregadod e salbado de tfstate

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-16-02-2025"
+    key            = "infraestructura/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
